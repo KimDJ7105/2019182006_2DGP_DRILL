0f99374f255b10c54377a9d2c5834d0ee8e43a0e
KimDJ7105 <rlaehdwo1225@naver.com>
2022-09-22 15:24:29 +0900

    Drill 06 완성
    2019182006 김동재

---
+++ 06/character_grass.py
@@ -0,0 +1,51 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+rec = True
+
+while (True):
+    if rec is False :
+        x = 0
+        y = 90
+        while (x < 770) :
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,90)
+            x = x + 2
+            delay(0.01)
+        while(y < 570) :
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            y = y + 2
+            delay(0.01)
+        while(x > 30) :
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            x = x - 2
+            delay(0.01)
+        while(y > 90) :
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            y = y - 2
+            delay(0.01)
+
+        rec = False
+    else :
+        angle = -90
+        while(angle < -82) :
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(400 + (math.cos(angle) * 200), 300 + (math.sin(angle)*200))
+            angle = angle + 0.05
+            delay(0.01)
+        rec = False
+
+close_canvas()
